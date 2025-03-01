5e8f0f0b51150d6ef4f3b31a59339e706f87652c
sameer-panwar <sameerpanwar9720@gmail.com>
2025-03-01 20:05:16 +0530

    learned and implemented tower of Hanoi

---
+++ 12-towerOfHanoi.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void towerofHanoi(int n, char src, char dest, char help){
+    if(n==0)return;
+
+    towerofHanoi(n-1, src, help, dest);
+    cout<<"move from "<<src<<" to "<<dest<<endl;
+    towerofHanoi(n-1, help, dest, src);
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    towerofHanoi(n, 'A', 'C', 'B');
+}
