7a3d1ebc0ce5d9c6d654d803c624aa8a96a6a2cd
sameer-panwar <sameerpanwar9720@gmail.com>
2025-03-07 12:17:04 +0530

    done with reversing a linked list with recursion method

--- 15-reverseLL.cpp
+++ 15-reverseLL.cpp
@@ -34,16 +34,6 @@ void insertAtHead(node* &head, int val){
     head=n;
 }
 
-int search(node* head, int key){
-    node* temp=head;
-    while(temp!=NULL){
-        if(temp->data==key){
-            return true;
-        }
-        temp=temp->next;
-    }
-    return false;
-}
 
 void display(node* head){
     node* temp=head;
@@ -54,53 +44,50 @@ void display(node* head){
     cout<<"NULL";
 }
 
-void deleteAtHead(node* &head){
-    node* toDelete=head;
 
-    head=head->next;
-    delete toDelete;
-}
 
-void deletion(node* head, int val){
 
-    if(head==NULL){
-        return;
-    }
+// node* reverse(node* &head){
+//     node* prevptr=NULL;
+//     node* currptr=head;
+//     node* nextptr;
 
-    if(head->next==NULL){
-        deleteAtHead(head);
-        return;
-    }
+//     while(currptr!=NULL){
+//         nextptr=currptr->next;
+//         currptr->next=prevptr;
+//         prevptr=currptr;
+//         currptr=nextptr;
+//     }
 
-    node* temp=head;
-    while(temp->next->data!=val){
-        temp=temp->next;
-    }
-
-    node* toDelete=temp->next;
-    temp->next=temp->next->next;
-    delete toDelete;
-}
+//     head=prevptr;
+//     return head;
+// }
 
 node* reverse(node* head){
-    
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+
+    node* newHead=reverse(head->next);
+    head->next->next=head;
+    head->next=NULL;
+
+    return newHead;
 }
 
 int main(){
     node* head=NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
     display(head);
     cout<<endl;
     insertAtHead(head, 0);
     insertAtHead(head, -1);
     display(head);
     cout<<endl;
-    cout<<search(head, 2)<<endl;
-    cout<<search(head, 5)<<endl;
-    cout<<search(head, -1)<<endl;
-
-    deletion(head, -1);
-    display(head);
+    node* newHead=reverse(head);
+    display(newHead);
     return 0;
 }
